bc1e6f0ebd376ba19d3033cce2862bef0d0e33bf
Швецов Александр <shvitc@gmail.com>
2025-04-28 00:50:27 +0300

    Сделали адаптив хедеру

--- src/app/ui/global-sections/Header.tsx
+++ src/app/ui/global-sections/Header.tsx
@@ -4,9 +4,11 @@ import {useEffect, useState} from "react";
 import Logo from "./Logo";
 import Section from "./Section";
 import nav from "@/app/ui/nav.json";
+import clsx from "clsx";
 
 const Header = () => {
 	const [hasScrolled, setHasScrolled] = useState(false);
+	const [isOpenedBurger, setIsOpenedBurger] = useState(false);
 
 	useEffect(() => {
 		const handleScroll = () => {
@@ -22,32 +24,93 @@ const Header = () => {
 	}, []);
 
 	return (
-		<div className='h-5000'>
-			<header
-				className={`flex items-center justify-center bg-white fixed w-full h-[100px] transition-shadow duration-200 ${
-					hasScrolled ? "shadow-lg" : "shadow-none"
-				}`}
-			>
-				<div className='w-full'>
-					<Section paddingY={0}>
-						<div className='flex items-center justify-between'>
-							<Logo />
-							<nav className='flex gap-[2.25rem]'>
-								{nav.map((navLink) => (
-									<a
-										className="relative after:content-[''] after:block after:absolute after:bottom-0 after:left-0 after:h-[1px] after:w-0 after:bg-black after:transition-all after:duration-200 hover:after:w-full"
-										href={`#${navLink.link}`}
-										key={navLink.link}
-									>
-										{navLink.text}
-									</a>
-								))}
-							</nav>
+		<header
+			className={`flex items-center justify-center bg-white fixed w-full h-[100px] transition-shadow duration-200 ${
+				hasScrolled ? "shadow-lg" : "shadow-none"
+			}`}
+		>
+			<div className='w-full'>
+				<Section paddingY={0}>
+					<div className='flex items-center justify-between'>
+						<Logo />
+						<nav className='flex gap-[2.25rem] max-lg:hidden'>
+							{nav.map((navLink) => (
+								<a
+									className="relative font-medium text-sm after:content-[''] after:block after:absolute after:bottom-0 after:left-0 after:h-[1px] after:w-0 after:bg-black after:transition-all after:duration-200 hover:after:w-full"
+									href={`#${navLink.link}`}
+									key={navLink.link}
+								>
+									{navLink.text}
+								</a>
+							))}
+						</nav>
+						<div className='hidden max-lg:block'>
+							<button
+								type='button'
+								className='flex items-center justify-center w-[60px] h-[60px] relative z-50 cursor-pointer'
+								onClick={() => setIsOpenedBurger((prev) => !prev)}
+							>
+								<div
+									className={clsx(
+										"absolute w-3/4 h-[3px] bg-black transition-[translate,rotate] duration-240",
+										{
+											"delay-[0ms,120ms] translate-y-0 rotate-45": isOpenedBurger,
+											"delay-[120ms,0ms] -translate-y-[13px] rotate-0": !isOpenedBurger,
+										}
+									)}
+								></div>
+								<div
+									className={clsx(
+										"absolute w-3/4 h-[3px] bg-black transition-opacity duration-0 delay-120",
+										{
+											"opacity-100": !isOpenedBurger,
+											"opacity-0": isOpenedBurger,
+										}
+									)}
+								></div>
+								<div
+									className={clsx(
+										"absolute w-3/4 h-[3px] bg-black transition-[translate,rotate] duration-240",
+										{
+											"delay-[0ms,120ms] translate-y-0 -rotate-45": isOpenedBurger,
+											"delay-[120ms,0ms] translate-y-[13px] rotate-0": !isOpenedBurger,
+										}
+									)}
+								></div>
+							</button>
+							<div className='fixed w-[100vw] min-h-[100vh] top-0 left-0 z-40'>
+								<div
+									onClick={() => setIsOpenedBurger((prev) => !prev)}
+									className={clsx(
+										"absolute inset-0 transition-colors duration-300",
+										isOpenedBurger
+											? "bg-black-opacity pointer-events-auto"
+											: "bg-[#00000000] pointer-events-none"
+									)}
+								></div>
+								<nav
+									className={clsx(
+										"flex flex-col items-center gap-5 absolute right-0 top-0 min-h-[100vh] py-25 px-35 bg-white transition-transform duration-300 max-sm:px-25",
+										isOpenedBurger ? "translate-x-[0%]" : "translate-x-[100%]"
+									)}
+								>
+									{nav.map((navLink) => (
+										<a
+											className='relative text-sm'
+											href={`#${navLink.link}`}
+											key={navLink.link}
+											onClick={() => setIsOpenedBurger((prev) => !prev)}
+										>
+											{navLink.text}
+										</a>
+									))}
+								</nav>
+							</div>
 						</div>
-					</Section>
-				</div>
-			</header>
-		</div>
+					</div>
+				</Section>
+			</div>
+		</header>
 	);
 };
 
