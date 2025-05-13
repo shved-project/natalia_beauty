4f9a12cafa9beefaf5c5c2236d924188d937e2f4
Швецов Александр <shvitc@gmail.com>
2025-05-13 22:30:00 +0300

    Сделали адаптив логотипу

--- src/app/ui/global-components/Logo.tsx
+++ src/app/ui/global-components/Logo.tsx
@@ -1,5 +1,5 @@
 const Logo = () => {
-	return <p className='uppercase font-semibold text-md'>Natalia beauty</p>;
+	return <p className='uppercase font-semibold text-md max-sm:text-sm'>Natalia beauty</p>;
 };
 
 export default Logo;
