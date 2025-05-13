7ced7f356f0d83f577479c07ef7448ab82d92317
Швецов Александр <shvitc@gmail.com>
2025-05-13 13:32:59 +0300

    Сделали адаптив в стандартных секциях

--- src/app/ui/global-sections/Section.tsx
+++ src/app/ui/global-sections/Section.tsx
@@ -5,7 +5,10 @@ interface SectionProps {
 
 const Section = ({children, paddingY = 228}: SectionProps) => {
 	return (
-		<div className='max-w-[1250px] mx-auto px-[20px]' style={{paddingBlock: `${paddingY}px`}}>
+		<div
+			className={`max-w-[1250px] mx-auto px-[20px] ${paddingY === 228 && "max-lg:!py-[80px]"}`}
+			style={{paddingBlock: `${paddingY}px`}}
+		>
 			{children}
 		</div>
 	);
