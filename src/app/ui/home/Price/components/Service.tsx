163bbc926746ebb3921d0e2d88dee267f67bb116
Швецов Александр <shvitc@gmail.com>
2025-05-16 17:18:01 +0300

    Изменили вид имён услуг

--- src/app/ui/home/Price/components/Service.tsx
+++ src/app/ui/home/Price/components/Service.tsx
@@ -12,7 +12,7 @@ const Service = ({name, price, image, additional}: ServiceProps) => {
 		<article>
 			<div className='w-full aspect-[310/346] relative rounded-[40px] overflow-hidden max-lg:aspect-square'>
 				<Image src={`${process.env.URL_API}${image}`} alt='' fill className='object-cover -z-10 scale-140' />
-				<div className='absolute bottom-[40px] left-0 bg-[#000000] text-white font-semibold px-[19px] py-[16px] rounded-[0_60px_60px_0]'>
+				<div className='flex items-center absolute h-[70px] bottom-0 left-0 right-0 bg-[#00000073] text-white font-semibold px-[18px]'>
 					{name}
 				</div>
 			</div>
